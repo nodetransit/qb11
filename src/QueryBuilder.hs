eed149ef9809e3ff7c103c7f5e9c0491410aa16c
frostbane <frostbane@programmer.net>
2023-01-14 00:36:52 +1000

    dev

--- src/QueryBuilder.hs
+++ src/QueryBuilder.hs
@@ -13,11 +13,23 @@ import Data.Text (Text)
 import Data.Array as Array
 
 
-data Column = Column Text
-            | Column Text Text
-            | RawColumn Text
+data Column = Column         Text
+            | ColumnAlias    Text Text
+            | RawColumn      Text
+            | RawColumnAlias Text Text
 
-data Condition = Condition Text | Array Condition
+data Operation = Equals
+               | NotEquals
+               | Null
+               | NotNull
+               | Like
+               | NotLike
+             deriving Show
+
+data Condition = Condition String Operation String
+               | Group [Condition]
+             deriving Show
+-- data Condition = Condition Text | Array Condition
 
 data Query = Query
     { query_table      :: Text
