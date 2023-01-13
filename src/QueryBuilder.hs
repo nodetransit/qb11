95eddfefa218293303fe83e95d095e6767be3749
frostbane <frostbane@programmer.net>
2023-01-14 00:36:52 +1000

    dev

--- src/QueryBuilder.hs
+++ src/QueryBuilder.hs
@@ -3,34 +3,41 @@
 
 module QueryBuilder
     ( Query (..)
+    , Join (..)
+    , Column (..)
     , createQuery
     ) where
 
 import Data.Text as T
 import Data.Text (Text)
+import Data.Array as Array
 
 
-newtype Query = Query
-    { table      :: Text
-    , conditions :: Condition
-    , bindings   :: [a]
-    , orderBy    :: [a]
-    , distinct   :: [b]
-    , limit      :: [n]
-    , joins      :: [a]
+data Column = Column Text
+            | Column Text Text
+            | RawColumn Text
+
+data Condition = Condition Text | Array Condition
+
+data Query = Query
+    { query_table      :: Text
+    , query_conditions :: Condition
+    -- , query_bindings   :: [a]
+    -- , query_orderBy    :: [a]
+    -- , query_distinct   :: [b]
+    -- , query_limit      :: [n]
+    -- , query_joins      :: [a]
     }
 
-newtype Join = Join
-    { table      :: Text
-    , alias      :: Text
-    , bindings   :: [Text]
-    , conditions :: Condition
+data Join = Join
+    { join_table      :: Text
+    , join_alias      :: Text
+    -- , join_bindings   :: Array Text
+    , join_conditions :: Condition
     }
-
-data Condition = Condition | [Condition] | and | or
 
 createQuery :: Text -> Query
-createQuery table = Query { table      = table
-                          , conditions = []
-                          }
+createQuery tableName = Query { query_table      = tableName
+                              -- , query_conditions = []
+                              }
 
