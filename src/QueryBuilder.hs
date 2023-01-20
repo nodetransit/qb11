6a34ddc0a5b3b2e2ee1884a27c5c286fc1c86928
ǝuɐqʇsoɹɟ <uod@uqso>
2023-01-21 09:55:53 +1000

    dev

--- src/QueryBuilder.hs
+++ src/QueryBuilder.hs
@@ -11,42 +11,10 @@ module QueryBuilder
 import Data.Text as T
 import Data.Text (Text)
 import Data.Array as Array
+import QueryBuilder.Types
 
 
-data Column = Column         Text
-            | ColumnAlias    Text Text
-            | RawColumn      Text
-            | RawColumnAlias Text Text
-
-data Operation = Equals
-               | NotEquals
-               | Null
-               | NotNull
-               | Like
-               | NotLike
-             deriving Show
-
-data Condition = Condition String Operation String
-               | Group [Condition]
-             deriving Show
--- data Condition = Condition Text | Array Condition
-
-data Query = Query
-    { query_table      :: Text
-    , query_conditions :: Condition
-    -- , query_bindings   :: [a]
-    -- , query_orderBy    :: [a]
-    -- , query_distinct   :: [b]
-    -- , query_limit      :: [n]
-    -- , query_joins      :: [a]
-    }
-
-data Join = Join
-    { join_table      :: Text
-    , join_alias      :: Text
-    -- , join_bindings   :: Array Text
-    , join_conditions :: Condition
-    }
+data QueryBuilderT q m b = QueryBuilderT { runQueryBuilderT :: m (q, b) }
 
 createQuery :: Text -> Query
 createQuery tableName = Query { query_table      = tableName
