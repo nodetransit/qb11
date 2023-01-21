90bbbf629e0558b0b65c189eee267b755b562f39
ǝuɐqʇsoɹɟ <uod@uqso>
2023-01-21 16:28:21 +1000

    concatenate table name

--- src/QueryBuilder/Types.hs
+++ src/QueryBuilder/Types.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module QueryBuilder.Types
     ( Query(..)
@@ -28,6 +29,7 @@ data Column = Column         Text
             | ColumnAlias    Text Text
             | RawColumn      Text
             | RawColumnAlias Text Text
+            deriving Show
 
 data Operation = Equals
                | NotEquals
@@ -48,13 +50,14 @@ data Query = EmptyQuery
            | Query { query_type       :: Text
                    , query_table      :: Text
                    , query_columns    :: [Column]
-                   -- , query_conditions :: Condition
-                   -- , query_bindings   :: [a]
-                   -- , query_orderBy    :: [a]
-                   -- , query_distinct   :: [b]
-                   -- , query_limit      :: [n]
-                   -- , query_joins      :: [a]
-                   }
+               -- , query_conditions :: Condition
+               -- , query_bindings   :: [a]
+               -- , query_orderBy    :: [a]
+               -- , query_distinct   :: [b]
+               -- , query_limit      :: [n]
+               -- , query_joins      :: [a]
+               }
+            deriving Show
 
 defaultQuery = Query { query_type    = ""
                      , query_table   = ""
@@ -73,12 +76,25 @@ table = Table
 columns = Columns
 
 modify_query :: Query -> Query -> Query
-modify_query EmptyQuery   q           = q
-modify_query q@(Query {}) (Table t)   = q { query_table = t }
-modify_query q@(Query {}) (Columns c) = q { query_columns = c }
-modify_query q@(Query {}) _           = q
-modify_query (Columns c)  (Table t)   = (defaultQuery { query_columns = c }) { query_table = t }
-modify_query _            _           = EmptyQuery
+modify_query EmptyQuery     q              = q
+modify_query q              EmptyQuery     = q
+modify_query q@(Query {})   (Table t)      = q { query_table = t }
+modify_query q@(Query {})   (Columns c)    = q { query_columns = c }
+modify_query (Columns c)    q              = defaultQuery { query_columns = c } <> q
+modify_query (Table t)      q              = defaultQuery { query_table = t } <> q
+modify_query qL             qR             = coalesceQuery qL qR
+
+coalesceQuery :: Query -> Query -> Query
+coalesceQuery qL qR = Query { query_type    = queryType
+                            , query_table   = queryTable
+                            , query_columns = queryColumns
+                            }
+  where
+    coalesce f a b = if f a /= 0 then a else b
+
+    queryType    = coalesce T.length (query_type qL) (query_type qR)
+    queryTable   = coalesce T.length (query_table qL) (query_table qR)
+    queryColumns = coalesce Prelude.length (query_columns qL) (query_columns qR)
 
 instance Semigroup Query where
     (<>) = modify_query
--- test/Spec/Query.hs
+++ test/Spec/Query.hs
@@ -18,7 +18,8 @@ runColumnSpec =
       it "update constructor" $ getColumnCount update `shouldBe` 0
       it "delete constructor" $ getColumnCount delete `shouldBe` 0
     context "building a full query should be valid" $ do
-       it "select query" $ checkSelectQuery `shouldBe` True
+       it "select query in order" $ checkSelectQuery `shouldBe` True
+       it "select query not in order" $ checkSelectQueryNotInOrder `shouldBe` True
 
 -- |
 getColumnCount :: Query -> Int
@@ -27,10 +28,19 @@ getColumnCount = length . query_columns
 -- |
 checkSelectQuery :: Bool
 checkSelectQuery =  query_type q == "SELECT"
-                 -- && query_table q == "users"
+                 && query_table q == "users"
                  && query_columns q `isSameColumns` [Column "id", Column "name"]
   where
     q =  select
-      -- <> from "users"
+      <> from "users"
       <> columns [Column "id", Column "name"]
+
+checkSelectQueryNotInOrder :: Bool
+checkSelectQueryNotInOrder =  query_type q == "SELECT"
+                           && query_table q == "users"
+                           && query_columns q `isSameColumns` [Column "id", Column "name"]
+  where
+    q =  columns [Column "id", Column "name"]
+      <> from "users"
+      <> select
 
