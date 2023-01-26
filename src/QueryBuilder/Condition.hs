5a7b0d3b27a8b432b086f9ce7ef4e948af177ebc
frostbane <frostbane@programmer.net>
2023-01-26 16:09:33 +1000

    monoid instance of condition => CT m b

--- src/QueryBuilder/Condition.hs
+++ src/QueryBuilder/Condition.hs
@@ -16,6 +16,7 @@ import Data.Text as T
 import Data.Text (Text)
 import Control.Monad
 import Control.Applicative
+import Data.Functor.Identity
 import Prelude hiding (and)
 
 data Condition = Condition Text Operation Text
@@ -39,42 +40,46 @@ instance Show Operation where
     show Like      = "LIKE"
     show NotLike   = "NOT LIKE"
 
-data ConditionT a m b = ConditionT { runConditionT :: m (b, a) }
+data ConditionT m b = ConditionT { runConditionT :: m b }
 
-instance (Functor m) => Functor (ConditionT b m) where
-    fmap f = mapConditionT $ fmap $ \(b, w) -> (f b, w)
+instance (Functor m) => Functor (ConditionT m) where
+    fmap :: (a -> b) -> ConditionT m a -> ConditionT m b
+    fmap f = mapConditionT $ fmap $ \b -> f b
       where
         mapConditionT f m = ConditionT $ f (runConditionT m)
 
-instance (Monoid b, Applicative m) => Applicative (ConditionT b m) where
-    pure b = ConditionT $ pure (b, mempty)
+instance (Applicative m) => Applicative (ConditionT m) where
+    pure :: b -> ConditionT m b
+    pure b = ConditionT $ pure b
 
-    (<*>) f v = ConditionT $ liftA2 k f' v'
+    (<*>) :: ConditionT m (a -> b) -> ConditionT m a -> ConditionT m b
+    (<*>) f v = ConditionT $ do
+        f' <*> v'
       where
-        k (b, a) (b', a') = (b b', a <> a')
         f' = runConditionT f
         v' = runConditionT v
- 
-instance (Monoid a, Monad m) => Monad (ConditionT a m) where
-    return :: b -> ConditionT a m b
-    -- return b = ConditionT $ \b -> return (b, mempty)
-    return b = (ConditionT . return) (b, mempty)
 
-    -- (>>=) :: ConditionT a m b -> (a -> ConditionT a m b) -> ConditionT a m b
+instance (Monad m) => Monad (ConditionT m) where
+    return :: b -> ConditionT m b
+    -- return b = ConditionT $ \b -> return (b, mempty)
+    return b = (ConditionT . return) b
+
+    -- (>>=) :: ConditionT m b -> (b -> ConditionT m b) -> ConditionT m b
     (>>=) m k = ConditionT $ do
-        (b, a) <- runConditionT m
-        (b', a') <- runConditionT (k b)
-        return (b', a <> a')
+        b <- runConditionT m
+        b' <- runConditionT (k b)
+        return b'
 
 -- and :: Condition -> Text -> W Text
 -- and (Condition a b c) d = W (d <> " " <> a <> (T.pack $ show b) <> "?", [c])
--- 
--- main :: W Text
--- main = do
---     f
---     and $ Condition "id" Equals "1"
---   where
---     f = return "where"
+
+main :: ConditionT Identity (Text, [Text])
+main = do
+    return ("?", ["a"])
+--    f
+--    and $ Condition "id" Equals "1"
+--  where
+--    f = return "where"
 
 -- and_ :: Bool
 -- and_ = True
