267a61de0a681a68f9b0a2199d7bbb1a4e975b3b
frostbane <frostbane@programmer.net>
2023-02-13 00:30:05 +1000

    test permutation

--- test/Spec/Query.hs
+++ test/Spec/Query.hs
@@ -6,7 +6,11 @@ module Spec.Query
     ) where
 
 import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
 import Spec.Util
+import Control.Monad
+import Data.List hiding (and, or)
 import Prelude hiding (and, or, null, not)
 
 import QueryBuilder.Query
@@ -26,21 +30,25 @@ queryColumnSpec =
       let q = checkSelectQuery
       it "query type" $ query_type q `shouldBe` "SELECT"
       it "query table" $ query_table q `shouldBe` "users"
-      it "query columns" $ query_columns q `isSameColumns` [Column "id", Column "name"]
+      it "query columns" $ query_columns q `shouldBeTheSameColumns` [Column "id", Column "name"]
       it "query conditions" $ (clause . query_conditions) q `shouldBe` "deleted <> ? OR deleted IS NOT NULL"
       it "query conditions" $ (bindings . query_conditions) q `shouldBe` [""]
       it "query order by" $ query_orderBy q `shouldBe` Asc
       it "query group by" $ query_groupBy q `shouldBe` [Column "type", Column "access"]
 
-    context "building a full query in reversed order should be valid" $ do
-      let q = checkSelectQueryNotInOrder
-      it "query order by" $ query_groupBy q `shouldBe` [Column "genre"]
-      it "query order by" $ query_orderBy q `shouldBe` Desc
-      it "query conditions" $ (clause . query_conditions) q `shouldBe` "released <> ? AND released IS NOT NULL"
-      it "query conditions" $ (bindings . query_conditions) q `shouldBe` [""]
-      it "query columns" $ query_columns q `isSameColumns` [Column "id", Column "title"]
-      it "query table" $ query_table q `shouldBe` "albums"
-      it "query type" $ query_type q `shouldBe` "SELECT"
+    context "building a full query in any order should be valid" $ do
+      forM_ (permutations checkSelectQueryNotInOrder) $
+        \queries -> do
+           let q = foldl' (<>) defaultQuery queries
+           it ("testing permutation :" ++ showQueries queries) $ do
+               query_groupBy q `shouldBe` [Column "genre"]
+               query_orderBy q `shouldBe` Desc
+               (clause . query_conditions) q `shouldBe` "released <> ? AND released IS NOT NULL"
+               (bindings . query_conditions) q `shouldBe` [""]
+               query_table q `shouldBe` "albums"
+               query_type q `shouldBe` "SELECT"
+           prop ("testing permutation :" ++ showQueries queries) $ do
+               query_columns q `shouldBeTheSameColumns` [Column "id", Column "title"]
 
 
 -- |
@@ -60,15 +68,16 @@ checkSelectQuery =
     <> OrderBy Asc
     <> GroupBy [Column "type", Column "access" ]
 
-checkSelectQueryNotInOrder :: Query
+checkSelectQueryNotInOrder :: [Query]
 checkSelectQueryNotInOrder =
-    GroupBy [Column "genre"]
-    <> OrderBy Desc
-    <> Where (runCondition $ do
-        condition "released" (notEquals "")
-        and "released" isNotNull
-    )
-    <> Columns [Column "id", Column "title"]
-    <> From "albums"
-    <> Select
+    [ GroupBy [Column "genre"]
+    , OrderBy Desc
+    , Where (runCondition $ do
+          condition "released" (notEquals "")
+          and "released" isNotNull
+      )
+    , Columns [Column "id", Column "title"]
+    , From "albums"
+    , Select
+    ]
 
--- test/Spec/Util.hs
+++ test/Spec/Util.hs
@@ -1,9 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Spec.Util
     ( isEmptyQuery
-    , isSameColumns
+    , shouldBeTheSameColumns
+    , showQuery
+    , showQueries
     ) where
 
+-- import Data.Text as T hiding (map, length, all, zipWith)
+-- import Data.Text (Text)
+import Data.List
+import Test.QuickCheck
 import QueryBuilder.Query
+
+showQuery :: Query -> String
+showQuery q = f q
+  where
+    f EmptyQuery  = "EmptyQuery"
+    f Select      = "Select"
+    f Insert      = "Insert"
+    f Update      = "Update"
+    f Delete      = "Delete"
+    f (From _)    = "From"
+    f (Table _)   = "Table"
+    f (Into _)    = "Into"
+    f (Columns _) = "Columns"
+    f (Where _)   = "Where"
+    f (OrderBy _) = "OrderBy"
+    f (GroupBy _) = "GroupBy"
+    f _           = "?"
+
+showQueries qs = intercalate ", " $ map showQuery qs
 
 -- | check if query is an EmptyQuery
 isEmptyQuery :: Query -> Bool
@@ -11,14 +38,14 @@ isEmptyQuery EmptyQuery = True
 isEmptyQuery _          = True
 
 -- | compare if the array is similar
-isSameColumns :: [Column] -> [Column] -> Bool
-isSameColumns csL csR | length csL /= length csR = False
-                      | otherwise = all (==True) $ zipWith compareColumn csL csR
+shouldBeTheSameColumns :: [Column] -> [Column] -> Property
+shouldBeTheSameColumns csL csR | length csL /= length csR = property $ False
+                      | otherwise                = property $ all (==True) $ zipWith compareColumn csL csR
                     where
                       compareColumn :: Column -> Column -> Bool
                       compareColumn (Column cl)            (Column cr)            = cl == cr
                       compareColumn (ColumnAlias cl al)    (ColumnAlias cr ar)    = cl == cr && al == ar
                    -- compareColumn (RawColumn cl)         (RawColumn cr)         = cl == cr
                    -- compareColumn (RawColumnAlias cl al) (RawColumnAlias cr ar) = cl == cr && al == ar
-                      compareColumn _                   _                         = False
+                      compareColumn _                      _                      = False
 
