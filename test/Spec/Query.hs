bcc7b67d6f5d8215d068eaf9f438eef8f246185f
ǝuɐqʇsoɹɟ <uod@uqso>
2023-02-14 16:15:44 +1000

    having

--- test/Spec/Query.hs
+++ test/Spec/Query.hs
@@ -35,6 +35,8 @@ queryColumnSpec =
       it "query conditions" $ (bindings . query_conditions) q `shouldBe` [""]
       it "query order by" $ query_orderBy q `shouldBe` Asc
       it "query group by" $ query_groupBy q `shouldBe` [Column "type", Column "access"]
+      it "query having conditions" $ (clause . query_having) q `shouldBe` "type LIKE ?"
+      it "query having conditions" $ (bindings . query_having) q `shouldBe` ["%admin%"]
 
     context "building a full query in any order should be valid" $ do
       forM_ (permutations checkSelectQueryNotInOrder) $
@@ -47,6 +49,8 @@ queryColumnSpec =
                (bindings . query_conditions) q `shouldBe` [""]
                query_table q `shouldBe` "albums"
                query_type q `shouldBe` "SELECT"
+               (clause . query_having) q `shouldBe` "genre LIKE ?"
+               (bindings . query_having) q `shouldBe` ["%prog%"]
            prop ("testing permutation :" ++ showQueries queries) $ do
                query_columns q `shouldBeTheSameColumns` [Column "id", Column "title"]
 
@@ -59,25 +63,31 @@ getColumnCount = length . query_columns . (EmptyQuery <>)
 checkSelectQuery :: Query
 checkSelectQuery =
     Select
-    <> From "users"
     <> Columns [Column "id", Column "name"]
+    <> From "users"
     <> Where (runConditionM $ do
         condition "deleted" (notEquals "")
         or "deleted" isNotNull
     )
-    <> OrderBy Asc
     <> GroupBy [Column "type", Column "access" ]
+    <> Having (runConditionM $ do
+        condition "type" (like "%admin%")
+    )
+    <> OrderBy Asc
 
 checkSelectQueryNotInOrder :: [Query]
 checkSelectQueryNotInOrder =
-    [ GroupBy [Column "genre"]
-    , OrderBy Desc
+    [ Select
+    , Columns [Column "id", Column "title"]
+    , From "albums"
     , Where (runConditionM $ do
           condition "released" (notEquals "")
           and "released" isNotNull
       )
-    , Columns [Column "id", Column "title"]
-    , From "albums"
-    , Select
+    , GroupBy [Column "genre"]
+    , Having (runConditionM $ do
+        condition "genre" (like "%prog%")
+    )
+    , OrderBy Desc
     ]
 
