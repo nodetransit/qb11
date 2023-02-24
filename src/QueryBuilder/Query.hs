056aad1d75bb645037f2c8daa47f0ebc44acf66a
ǝuɐqʇsoɹɟ <uod@uqso>
2023-02-24 15:35:08 +1000

    test query transformer

--- src/QueryBuilder/Query.hs
+++ src/QueryBuilder/Query.hs
@@ -4,7 +4,14 @@
 {-# LANGUAGE RecordWildCards #-}
 
 module QueryBuilder.Query
-    ( 
+    ( Internal.Query(..)
+    , QueryT
+    , runQueryT
+    , select
+    , from
+    , table
+    , into
+    , columns
     ) where
 
 import Data.Text as T
@@ -12,5 +19,29 @@ import Data.Text (Text)
 import Control.Monad
 import Control.Applicative
 
-import QueryBuilder.Internal.Query
+import qualified QueryBuilder.Internal.Query as Internal
+import QueryBuilder.Column
 
+type Query = Internal.Query
+type QueryT m = Internal.QueryT Query m Bool
+
+runQueryT :: (Monad m) => Internal.QueryT a m b -> m a
+runQueryT q = (return .snd) =<< Internal.runQueryT q
+
+select :: (Monad m) => QueryT m
+select = Internal.QueryT $ do
+    return (True, Internal.defaultQuery <> Internal.Select)
+
+from :: (Monad m) => Text -> QueryT m
+from t = Internal.QueryT $ do
+    return (True, Internal.defaultQuery <> Internal.Table t)
+
+-- | from aliases
+table :: (Monad m) => Text -> QueryT m
+table = from
+into :: (Monad m) => Text -> QueryT m
+into = from
+
+columns :: (Monad m) => [Column] -> QueryT m
+columns c = Internal.QueryT $ do
+    return (True, Internal.defaultQuery <> Internal.Columns c)
--- test/Spec/QueryT.hs
+++ test/Spec/QueryT.hs
@@ -5,19 +5,39 @@ module Spec.QueryT
     ( queryTSpec
     ) where
 
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
+
 import Prelude hiding (and, or, null, (&&), (||))
 import Data.Text as T hiding (null)
-import Test.Hspec
 import Control.Monad
 import Control.Monad.Identity
 import System.IO.Unsafe
 
+import Spec.Util
+
 import QueryBuilder.Query
+import QueryBuilder.Column
 
 queryTSpec :: Spec
 queryTSpec =
-    describe "query transformer" $ do
-      context "simple query" $ do
-        it "using identity monad" $ do
-          False `shouldNotBe` False
+  describe "query transformer" $ do
+    context "simple query" $ do
+
+      let q = testQueryTransformer
+      it "using identity monad" $ do
+        query_type q `shouldBe` "SELECT"
+        query_table q `shouldBe` "users"
+      prop "using identity monad" $ do
+        query_columns q `shouldBeTheSameColumns` [Column "id", Column "name", Column "level"]
+
+testQueryTransformer :: Query
+testQueryTransformer = (runIdentity . runQueryT) createQuery
+  where
+    createQuery :: QueryT Identity
+    createQuery = do
+        select
+        from "users"
+        columns [Column "id", Column "name", Column "level"]
 
