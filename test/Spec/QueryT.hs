2d95340d24b4951b69c3731373e2f48d83dce68b
frostbane <frostbane@programmer.net>
2023-02-27 17:57:28 +1000

    query re-exports
    for a cleaner import statement

--- test/Spec/QueryT.hs
+++ test/Spec/QueryT.hs
@@ -16,6 +16,7 @@ import Control.Monad.Identity hiding (join)
 import System.IO.Unsafe
 
 import Spec.Util
+import Spec.QueryTQueries
 
 import QueryBuilder.Query
 import QueryBuilder.Alias as Alias
@@ -94,68 +95,4 @@ queryTSpec =
         query_columns q `shouldBeTheSameColumns` [ColumnAlias "COUNT(id)" (As "count"), Column "country"]
       prop "query order by" $ do
         (order_columns . query_orderBy) q `shouldBeTheSameColumns` [Column "count"]
-
-testQueryTransformer :: Query
-testQueryTransformer = (runIdentity . runQueryT) createQuery
-  where
-    createQuery :: QueryT Identity
-    createQuery = do
-        select
-        from "users"
-        columns [ column "id"
-                , column "name"
-                , column "level"]
-        where_ $ do
-            condition "deleted" isNull
-            and_ $ do
-                condition "registered" isNotNull
-                or "validated" (equals true)
-            and "blocked" (equals false)
-        orderBy [ column "registered_on"
-                , column "last_login"
-                ] desc
-
-testQueryTransformerJoin :: Query
-testQueryTransformerJoin = (runIdentity . runQueryT) createQuery
-  where
-    createQuery :: QueryT Identity
-    createQuery = do
-        select
-        from "artists"
-        columns [ column "artists.id"
-                , column "sales.year"
-                , column "infos.album"
-                ]
-        join "sales" on $ do
-            condition "artists.id" (equalsRaw "sales.aid")
-        leftJoin "infos" on $ do
-            condition "artists.id" (equalsRaw "infos.aid")
-            and "infos.released" (equals true)
-        where_ $ do
-            condition "released" (equalsRaw "NOW()")
-
-testQueryTransformerJoinAs :: Query
-testQueryTransformerJoinAs =
-    runQuery $ do
-        select
-        from_ "customers" (as "c")
-        columns [ column "c.id"
-                , column "c.name"
-                , column "ci.address"]
-        rightJoin_ "infos" (as "ci") on $ do
-            condition "c.id" (equalsRaw "ci.customer_id")
-            and "c.valid" (equals true)
-
-testQueryGroupBy :: Query
-testQueryGroupBy =
-    runQuery $ do
-        select
-        from "users"
-        columns [ column_ "COUNT(id)" (as "count")
-                , column  "country"
-                ]
-        groupBy [column "country"]
-        having $ do
-            condition "count" (gte "5")
-        orderBy [column "count"] asc
 
---
+++ test/Spec/QueryTQueries.hs
@@ -0,0 +1,85 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS -Wno-missing-fields #-}
+
+module Spec.QueryTQueries
+    ( testQueryTransformer
+    , testQueryTransformerJoin
+    , testQueryTransformerJoinAs
+    , testQueryGroupBy
+    ) where
+
+import Prelude hiding (and, or, null, Left, Right)
+import Data.Text as T hiding (null, length, head, tail, groupBy)
+import Control.Monad hiding (join)
+import Control.Monad.Identity hiding (join)
+import System.IO.Unsafe
+
+import Spec.Util
+
+import QueryBuilder
+
+testQueryTransformer :: Query
+testQueryTransformer = (runIdentity . runQueryT) createQuery
+  where
+    createQuery :: QueryT Identity
+    createQuery = do
+        select
+        from "users"
+        columns [ column "id"
+                , column "name"
+                , column "level"]
+        where_ $ do
+            condition "deleted" isNull
+            and_ $ do
+                condition "registered" isNotNull
+                or "validated" (equals true)
+            and "blocked" (equals false)
+        orderBy [ column "registered_on"
+                , column "last_login"
+                ] desc
+
+testQueryTransformerJoin :: Query
+testQueryTransformerJoin = (runIdentity . runQueryT) createQuery
+  where
+    createQuery :: QueryT Identity
+    createQuery = do
+        select
+        from "artists"
+        columns [ column "artists.id"
+                , column "sales.year"
+                , column "infos.album"
+                ]
+        join "sales" on $ do
+            condition "artists.id" (equalsRaw "sales.aid")
+        leftJoin "infos" on $ do
+            condition "artists.id" (equalsRaw "infos.aid")
+            and "infos.released" (equals true)
+        where_ $ do
+            condition "released" (equalsRaw "NOW()")
+
+testQueryTransformerJoinAs :: Query
+testQueryTransformerJoinAs =
+    runQuery $ do
+        select
+        from_ "customers" (as "c")
+        columns [ column "c.id"
+                , column "c.name"
+                , column "ci.address"]
+        rightJoin_ "infos" (as "ci") on $ do
+            condition "c.id" (equalsRaw "ci.customer_id")
+            and "c.valid" (equals true)
+
+testQueryGroupBy :: Query
+testQueryGroupBy =
+    runQuery $ do
+        select
+        from "users"
+        columns [ column_ "COUNT(id)" (as "count")
+                , column  "country"
+                ]
+        groupBy [column "country"]
+        having $ do
+            condition "count" (gte "5")
+        orderBy [column "count"] asc
+
+
